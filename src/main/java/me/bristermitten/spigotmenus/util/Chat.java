3f808fd125e9c8a0b8bcc4550872eea0fc0fc02b
Alex Wood <alexljwood24@hotmail.co.uk>
2019-04-10 13:45:43 +0100

    Fix Chat#color not coloring ampersand codes

--- src/main/java/me/bristermitten/spigotmenus/util/Chat.java
+++ src/main/java/me/bristermitten/spigotmenus/util/Chat.java
@@ -5,7 +5,7 @@ import org.bukkit.ChatColor;
 public class Chat {
 
     public static String color(String s) {
-        if(s==null)return null;
-        return ChatColor.translateAlternateColorCodes(ChatColor.COLOR_CHAR, s);
+        if (s == null) return null;
+        return ChatColor.translateAlternateColorCodes('&', s);
     }
 }
