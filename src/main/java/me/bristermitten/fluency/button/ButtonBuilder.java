1b733f228f934226720ec9e8f3a0d7468e2b84f6
Alex <alexljwood24@hotmail.co.uk>
2019-08-11 17:12:33 +0100

    Rewrite of all main features complete

--- src/main/java/me/bristermitten/fluency/button/ButtonBuilder.java
+++ src/main/java/me/bristermitten/fluency/button/ButtonBuilder.java
@@ -4,11 +4,28 @@ import me.bristermitten.fluency.FluentBuilder;
 import me.bristermitten.fluency.button.click.HandlerBuilder;
 import me.bristermitten.fluency.menu.MenuBuilder;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+
+import java.util.List;
 
 public interface ButtonBuilder extends FluentBuilder<MenuButton, MenuBuilder> {
     ButtonBuilder amount(int amount);
 
     ButtonBuilder type(Material type);
+
+    ButtonBuilder data(short data);
+
+    ButtonBuilder name(String name);
+
+    ButtonBuilder lore(String... lore);
+
+    ButtonBuilder lore(List<String> lore);
+
+    ButtonBuilder addLore(String lore);
+
+    ButtonBuilder unbreakable();
+
+    ButtonBuilder enchant(Enchantment e, int level);
 
     HandlerBuilder onClick();
 
--- src/main/java/me/bristermitten/fluency/impl/ButtonBuilderImpl.java
+++ src/main/java/me/bristermitten/fluency/impl/ButtonBuilderImpl.java
@@ -1,11 +1,18 @@
 package me.bristermitten.fluency.impl;
 
 import me.bristermitten.fluency.Fluency;
+import me.bristermitten.fluency.Util;
 import me.bristermitten.fluency.button.ButtonBuilder;
 import me.bristermitten.fluency.button.MenuButton;
 import me.bristermitten.fluency.button.click.HandlerBuilder;
 import me.bristermitten.fluency.menu.MenuBuilder;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
 
 class ButtonBuilderImpl implements ButtonBuilder {
     private final Fluency fluency;
@@ -28,6 +35,58 @@ class ButtonBuilderImpl implements ButtonBuilder {
     public ButtonBuilder type(Material type) {
         button.setType(type);
         return this;
+    }
+
+    @Override
+    public ButtonBuilder data(short data) {
+        button.setDurability(data);
+        return this;
+    }
+
+    @Override
+    public ButtonBuilder name(String name) {
+        transformMeta(m -> m.setDisplayName(Util.color(name)));
+        return this;
+    }
+
+    @Override
+    public ButtonBuilder lore(String... lore) {
+        return lore(Arrays.asList(lore));
+    }
+
+    @Override
+    public ButtonBuilder lore(List<String> lore) {
+        lore.stream().map(Util::color).forEach(this::addLore);
+        return this;
+    }
+
+    @Override
+    public ButtonBuilder addLore(String lore) {
+        transformMeta(m -> {
+            List<String> l = m.getLore();
+            l.add(Util.color(lore));
+            m.setLore(l);
+        });
+        return this;
+    }
+
+    @Override
+    public ButtonBuilder unbreakable() {
+        transformMeta(m -> m.spigot().setUnbreakable(true));
+        return this;
+
+    }
+
+    @Override
+    public ButtonBuilder enchant(Enchantment e, int level) {
+        button.addEnchantment(e, level);
+        return this;
+    }
+
+    private void transformMeta(Consumer<ItemMeta> f) {
+        ItemMeta itemMeta = button.getItemMeta();
+        f.accept(itemMeta);
+        button.setItemMeta(itemMeta);
     }
 
     @Override
