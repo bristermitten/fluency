faf1dd5ff3a13f0b797286a0b96d4a49da790e7e
Alex Wood <alexljwood24@hotmail.co.uk>
2019-08-28 12:08:59 +0100

    Cancelling bug fixed

--- src/main/java/me/bristermitten/fluency/ClickListener.java
+++ src/main/java/me/bristermitten/fluency/ClickListener.java
@@ -14,13 +14,11 @@ public class ClickListener implements Listener {
         if (e instanceof MenuClickEvent) return;
         if (!(e.getWhoClicked() instanceof Player)) return;
         if (e.getClickedInventory() == null) return;
+        if (!(e.getWhoClicked().getOpenInventory().getTopInventory() instanceof MenuHolder)) return;
         if (e.getClickedInventory().equals(e.getWhoClicked().getInventory())) {
             e.setCancelled(true);
             return;
         }
-
-        if (!(e.getClickedInventory().getHolder() instanceof MenuHolder)) return;
-
 
         MenuHolder holder = (MenuHolder) e.getClickedInventory().getHolder();
         if (holder == null) return;
