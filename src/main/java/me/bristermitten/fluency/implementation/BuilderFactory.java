57fb1b682177d13b3a34bef69fb567ef8f874bf6
Alex Wood <alexljwood24@hotmail.co.uk>
2020-04-08 17:12:44 +0100

    Suppress Raw types and Unchecked Cast warnings

--- src/main/java/me/bristermitten/fluency/implementation/BuilderFactory.java
+++ src/main/java/me/bristermitten/fluency/implementation/BuilderFactory.java
@@ -9,33 +9,34 @@ import me.bristermitten.fluency.menu.MenuBuilder;
 import org.bukkit.entity.Player;
 
 public class BuilderFactory {
-	private final Fluency fluency;
+    private final Fluency fluency;
 
-	public BuilderFactory(Fluency fluency) {
-		this.fluency = fluency;
-	}
+    public BuilderFactory(Fluency fluency) {
+        this.fluency = fluency;
+    }
 
-	public MenuBuilder buildMenu() {
-		return new MenuBuilderImpl(fluency);
-	}
+    public MenuBuilder buildMenu() {
+        return new MenuBuilderImpl(fluency);
+    }
 
-	public ButtonBuilder buildButton() {
-		return buildButton(null);
-	}
+    public ButtonBuilder buildButton() {
+        return buildButton(null);
+    }
 
-	public ButtonBuilder buildButton(MenuBuilder parent) {
-		return new ButtonBuilderImpl(fluency, parent);
-	}
+    public ButtonBuilder buildButton(MenuBuilder parent) {
+        return new ButtonBuilderImpl(fluency, parent);
+    }
 
-	public <P extends FluentBuilder<?, ?>> HandlerBuilder<P> buildHandler(P parent) {
-		return new HandlerBuilderImpl(parent);
-	}
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    public <P extends FluentBuilder<?, ?>> HandlerBuilder<P> buildHandler(P parent) {
+        return new HandlerBuilderImpl(parent);
+    }
 
-	public <T> TemplateBuilder<T> buildTemplate(MenuBuilder parent) {
-		return new TemplateBuilderImpl<>(fluency, parent);
-	}
+    public <T> TemplateBuilder<T> buildTemplate(MenuBuilder parent) {
+        return new TemplateBuilderImpl<>(fluency, parent);
+    }
 
-	public TemplateBuilder<Player> buildViewerTemplate(MenuBuilder parent) {
-		return new PlayerTemplateBuilder(fluency, parent);
-	}
+    public TemplateBuilder<Player> buildViewerTemplate(MenuBuilder parent) {
+        return new PlayerTemplateBuilder(fluency, parent);
+    }
 }
