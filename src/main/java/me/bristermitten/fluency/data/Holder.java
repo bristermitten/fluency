651a90991c891260eb3026546fefa9b3f3264eb4
Alex Wood <alexljwood24@hotmail.co.uk>
2020-04-10 17:35:39 +0100

    Add Holder#transform

--- src/main/java/me/bristermitten/fluency/data/Holder.java
+++ src/main/java/me/bristermitten/fluency/data/Holder.java
@@ -1,46 +1,54 @@
 package me.bristermitten.fluency.data;
 
-import org.jetbrains.annotations.Nullable;
-
 import java.util.Objects;
+import java.util.function.Consumer;
 
 public class Holder<T> {
-    private T t;
+    private T value;
 
-	public Holder( T t) {
-		this.t = t;
-	}
+    public Holder(T value) {
+        this.value = value;
+    }
 
 
-	public T get() {
-		return t;
-	}
+    public T get() {
+        return value;
+    }
 
-	public void set(T t) {
-		this.t = t;
-	}
+    public void set(T t) {
+        this.value = t;
+    }
 
-	public boolean has() {
-		return t != null;
-	}
+    public boolean has() {
+        return value != null;
+    }
 
-	@Override
-	public String toString() {
-		return "Holder{" +
-				"var=" + t +
-				'}';
-	}
+    @Override
+    public String toString() {
+        return "Holder{" +
+                "var=" + value +
+                '}';
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		Holder<?> holder = (Holder<?>) o;
-		return Objects.equals(t, holder.t);
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Holder<?> holder = (Holder<?>) o;
+        return Objects.equals(value, holder.value);
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(t);
-	}
+    public void transform(Consumer<T> transformer) {
+        if (!has()) {
+            return;
+        }
+        T value = get();
+        transformer.accept(value);
+        set(value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
 }
